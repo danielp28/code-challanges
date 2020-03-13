1174bc9987d38ca94f663b90b284690cb76da4e0
Daniel <daniel.p.masters@gmail.com>
2020-03-13 10:53:45 -0500

    playing with mar11 challange more

--- Mar11/script.js
+++ Mar11/script.js
@@ -1,26 +1,31 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "tomsawyer",
-  database: "kill_trakerdb"
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-
-
-    connection.query("SELECT * FROM gameTracker", function (err, result){
-        if(err) throw err;
-        console.log(result)
-    });
+$("#submit").on("click", function(e){
+  e.preventDefault();
+  var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "tomsawyer",
+    database: "kill_trakerdb"
   });
+  
+  connection.connect(function(err) {
+      if (err) throw err;
+  
+  
+      connection.query("SELECT * FROM gameTracker", function (err, result){
+          if(err) throw err;
+          console.log(result)
+      });
+    });
+} )
+
 
