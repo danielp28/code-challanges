c45935f871e34a12a7d585537d6bc9040ea663fa
Daniel <daniel.p.masters@gmail.com>
2020-03-28 10:49:37 -0500

    Mar 28: An express app to take notes for levels in Doom Eternal

---
+++ MAr28/data/levelData.js
@@ -0,0 +1,13 @@
+var levelData = [
+    {
+        levelNumber: "1",
+        levelName: "Hell on Earth",
+        arenas: "5",
+        notes: "Use sticky bombs for cacodemons and full auto for arachnetrons"
+
+        
+    }
+];
+
+
+module.exports = levelData
